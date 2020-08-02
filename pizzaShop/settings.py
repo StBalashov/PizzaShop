0aabacd1282579b0cc4d75a2af0782da140c8ada
StBalashov <stepka.balashov@gmail.com>
2020-08-03 04:49:37 +0500

    initial commit

--- pizzaShop/settings.py
+++ pizzaShop/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
